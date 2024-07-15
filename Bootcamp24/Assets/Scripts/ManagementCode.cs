ad18a46f387d50e88336245023e743bdb7304f64
Zehra Gürcan <111696861+ZehraGurcan@users.noreply.github.com>
2024-07-15 15:41:57 +0300

    .

---
+++ Bootcamp24/Assets/Scripts/ManagementCode.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Realtime;
+using Photon.Pun;
+
+public class ManagementCode : MonoBehaviourPunCallbacks
+{
+    
+    void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    
+    void Update()
+    {
+        
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("server'a girildi!");
+        PhotonNetwork.JoinLobby();
+    }
+
+    public override void OnJoinedLobby()
+    {
+        Debug.Log("Lobby'ye girildi!");
+
+        PhotonNetwork.JoinOrCreateRoom("oda", new RoomOptions { MaxPlayers = 5, IsOpen = true, IsVisible = true }, TypedLobby.Default);
+    }
+
+    public override void OnJoinedRoom()
+    {
+        Debug.Log("Odaya girildi!");
+
+        GameObject nesne = PhotonNetwork.Instantiate("Gamer", Vector3.zero, Quaternion.identity, 0, null);
+    }
+
+    public override void OnLeftRoom()
+    {
+        Debug.Log("Odadan Çýkýldý!");
+    }
+
+    public override void OnLeftLobby()
+    {
+        Debug.Log("Lobby'den Çýkýldý!");
+    }
+
+    public override void OnJoinRoomFailed(short returnCode, string message)
+    {
+        Debug.Log("Hata : Odaya Girilemedi!");
+    }
+
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        Debug.Log("Hata : Herhangi Bir Odaya Girilemedi!");
+    }
+
+    public override void OnCreateRoomFailed(short returnCode, string message)
+    {
+        Debug.Log("Hata : Oda oluþturulamadý!");
+    }
+
+
+}
